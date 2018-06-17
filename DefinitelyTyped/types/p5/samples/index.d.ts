20bd41b92960362f957e2f607d328b9dce9455a5
Pierre Krafft <kpierre@outlook.com>
2018-06-17 15:17:29 +0200

    Add script to extract samples
    Takes examples from p5 comments and emits TypeScript files

---
+++ DefinitelyTyped/types/p5/samples/index.d.ts
@@ -0,0 +1,6 @@
+// TypeScript Version: 2.4
+
+declare namespace __Dummy__ {
+}
+
+export = __Dummy__;
---
+++ src/gather-samples.js
@@ -0,0 +1,65 @@
+/// @ts-check
+
+function splitLines(text) {
+  const lines = [];
+
+  let m;
+  const reSplit = /(( *\* ?)?.*)(?:\r\n|\r|\n)/g;
+  while ((m = reSplit.exec(text)) != null) {
+    if (m.index === reSplit.lastIndex) {
+      reSplit.lastIndex++;
+    }
+
+    lines.push({
+      index: m.index,
+      text: m[1],
+      prefixLength: m[2] ? m[2].length : 0
+    });
+  }
+
+  return lines;
+}
+
+const prologue = `import "p5/global";
+
+`;
+
+function processFile(text) {
+  this.lines = splitLines(text);
+
+  let m;
+  const comments = [];
+
+  const reComment = /\/\*\*(?:.|\r|\n)*?\*\//g;
+  while ((m = reComment.exec(text)) != null) {
+    let value = m[0];
+    comments.push({
+      value: value,
+      range: [m.index, m.index + value.length]
+    });
+  }
+
+  const samples = (this.samples = []);
+
+  for (let i = 0; i < comments.length; i++) {
+    const comment = comments[i];
+    const commentText = comment.value;
+
+    const re = /(<code[^>]*>\s*(?:\r\n|\r|\n))((?:.|\r|\n)*?)<\/code>/gm;
+    while ((m = re.exec(commentText)) != null) {
+      let code = m[2];
+      if (!code) continue;
+      code = code.replace(/^ *\* ?/gm, '');
+
+      samples.push({
+        comment: comment,
+        index: m.index + m[1].length,
+        code: code
+      });
+    }
+  }
+
+  return samples.map(s => `${prologue}${s.code}`);
+}
+
+module.exports = processFile;
