aacd0a9d9880b694aa82ef253391912cecb29024
wuyudi <asukaminato@duck.com>
2023-01-09 12:16:27 +0900

    shorten outdir

--- src/filepath.js
+++ src/filepath.js
@@ -2,5 +2,5 @@ const upath = require("upath");
 module.exports = {
 	datajsonfolder: upath.joinSafe(__dirname, "../datajson/"),
 	datajson: upath.joinSafe(__dirname, "../datajson/data.json"),
-	outdir: upath.joinSafe(__dirname, "../DefinitelyTyped/types/p5"),
+	outdir: upath.joinSafe(__dirname, "../types/p5"),
 };
