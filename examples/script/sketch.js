41b6349b30aa7f6f04dda1e19d0b0b597daea160
wuyudi <asukaminato@duck.com>
2023-01-14 02:37:46 +0900

    add info on readme

--- examples/script/sketch.js
+++ examples/script/sketch.js
@@ -1,10 +1,18 @@
 /// @ts-check
-/// <reference path="../../DefinitelyTyped/types/p5/global.d.ts" />
+/// <reference path="../../types/p5/global.d.ts" />
+
+const p5 = require("../../types/p5");
 
 function setup() {
 
 }
 
+/**
+ * 
+ * @param {p5.Vector} a 
+ */
+function foo(a){
+}
 function draw() {
   ellipse(50, 50, 80, 80);
 }
